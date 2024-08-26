21675450b0f121171782c103d7342cc820a3450e
yy <yyx_yyx@live.cn>
2024-08-26 20:57:36 +0800

    google a

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,23 @@
-import { Analytics } from '@vercel/analytics/react';
- 
+import Head from 'next/head';
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-01504QX7SD"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-01504QX7SD');
+          `
+        }} />
+      </Head>
       <Component {...pageProps} />
-      <Analytics />
     </>
   );
 }
- 
+
 export default MyApp;

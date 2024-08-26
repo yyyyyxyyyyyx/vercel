ef340f4fbdc9bc14fa2b65531a43c996ccb22988
yy <yyx_yyx@live.cn>
2024-08-26 20:49:37 +0800

    google

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,23 @@
-import type { AppProps } from 'next/app';
-import { Analytics } from '@vercel/analytics/react';
- 
-function MyApp({ Component, pageProps }: AppProps) {
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-01504QX7SD"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-01504QX7SD');
+          `
+        }} />
+      </Head>
       <Component {...pageProps} />
-      <Analytics />
     </>
   );
 }
- 
+
 export default MyApp;

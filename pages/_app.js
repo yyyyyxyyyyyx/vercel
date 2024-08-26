172be8f1965b2a3c1ab5e52a34b4d1fb132a9dc1
yy <yyx_yyx@live.cn>
2024-08-26 20:58:50 +0800

    v analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,12 @@
-import Head from 'next/head';
-
+import { Analytics } from '@vercel/analytics/react';
+ 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-01504QX7SD"></script>
-        <script dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-01504QX7SD');
-          `
-        }} />
-      </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
-
+ 
 export default MyApp;

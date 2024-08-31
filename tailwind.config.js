f89f91c4b1a1193c0e7cea8a21eced3b03b12096
yy <yyx_yyx@live.cn>
2024-08-31 14:19:43 +0800

    food stars

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html", // 包含 public 文件夹中的所有 HTML 文件
+    "./index.js",        // 包含根目录中的 index.js 文件
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
